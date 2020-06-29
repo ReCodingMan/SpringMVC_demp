c438cc086cd8a771362ec2fde3e84dd3aa6f493b
chengcheng1021 <chengcheng1021@aliyun.com>
2020-06-29 08:28:10 +0800

    获取原生servelt API

--- springmvc_day01_start/src/main/java/com/cc/controller/ParamController.java
+++ springmvc_day01_start/src/main/java/com/cc/controller/ParamController.java
@@ -5,6 +5,11 @@ import com.cc.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 /**
  * 请求参数绑定
  */
@@ -43,6 +48,25 @@ public class ParamController {
     public String saveAccount(User user) {
         System.out.println("执行了。。。。");
         System.out.println(user);
+        return "success";
+    }
+
+    /**
+     * 原生的API
+     * @return
+     */
+    @RequestMapping("/testServlet")
+    public String testServlet(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println("执行了。。。。");
+        System.out.println(request);
+
+        HttpSession session = request.getSession();
+        System.out.println(session);
+
+        ServletContext servletContext = session.getServletContext();
+        System.out.println(servletContext);
+
+        System.out.println(response);
         return "success";
     }
 }

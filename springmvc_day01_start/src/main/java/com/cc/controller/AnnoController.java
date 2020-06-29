18a3fca648309fbce80d1e4bb9e67668bedf571b
chengcheng1021 <chengcheng1021@aliyun.com>
2020-06-29 09:36:52 +0800

    pathVariable注解

--- springmvc_day01_start/src/main/java/com/cc/controller/AnnoController.java
+++ springmvc_day01_start/src/main/java/com/cc/controller/AnnoController.java
@@ -1,6 +1,7 @@
 package com.cc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,6 +34,18 @@ public class AnnoController {
     public String testRequestBody(@RequestBody String body) {
         System.out.println("执行了。。。");
         System.out.println(body);
+        return "success";
+    }
+
+    /**
+     * PathVariable 注解
+     * @param id
+     * @return
+     */
+    @RequestMapping("/testPathVariable/{sid}")
+    public String testPathVariable(@PathVariable(name = "sid") String id) {
+        System.out.println("执行了。。。");
+        System.out.println(id);
         return "success";
     }
 }
--- springmvc_day01_start/src/main/webapp/anno.jsp
+++ springmvc_day01_start/src/main/webapp/anno.jsp
@@ -22,5 +22,8 @@
         <input type="submit" value="提交" />
     </form>
 
+    <a href="anno/testPathVariable/10">PathVariable</a>
+
+
 </body>
 </html>

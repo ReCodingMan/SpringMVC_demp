64e0e55e66bf2a726332bb80034e6ea08489f590
chengcheng1021 <chengcheng1021@aliyun.com>
2020-06-29 08:42:49 +0800

    RequestBody 注解

--- springmvc_day01_start/src/main/java/com/cc/controller/AnnoController.java
+++ springmvc_day01_start/src/main/java/com/cc/controller/AnnoController.java
@@ -1,6 +1,7 @@
 package com.cc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,10 +12,27 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/anno")
 public class AnnoController {
 
+    /**
+     * RequestParam 注解
+     * @param username
+     * @return
+     */
     @RequestMapping("/testRequestParam")
     public String testRequestParam(@RequestParam(name = "name") String username) {
         System.out.println("执行了。。。");
         System.out.println(username);
+        return "success";
+    }
+
+    /**
+     * 获取请求体内容
+     * @param body
+     * @return
+     */
+    @RequestMapping("/testRequestBody")
+    public String testRequestBody(@RequestBody String body) {
+        System.out.println("执行了。。。");
+        System.out.println(body);
         return "success";
     }
 }
--- springmvc_day01_start/target/springmvc_day01_start/anno.jsp
+++ springmvc_day01_start/target/springmvc_day01_start/anno.jsp
@@ -11,8 +11,16 @@
     <title>Title</title>
 </head>
 <body>
+
 <%--    常用的注解--%>
 
     <a href="anno/testRequestParam?name=哈哈">RequestParam</a>
+
+    <form action="anno/testRequestBody" method="post">
+        用户姓名：<input type="text" name="username" /><br/>
+        用户年龄：<input type="text" name="age" /><br/>
+        <input type="submit" value="提交" />
+    </form>
+
 </body>
 </html>

8b6ec19f915a6eb460ce4e9c92af63a53fe75e1b
chengcheng1021 <chengcheng1021@aliyun.com>
2020-06-30 15:54:00 +0800

    SpringMVC 上传文件

--- springmvc_day02_fileupload/src/main/java/com/cc/controller/UserController.java
+++ springmvc_day02_fileupload/src/main/java/com/cc/controller/UserController.java
@@ -5,6 +5,7 @@ import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
@@ -16,12 +17,12 @@ import java.util.UUID;
 public class UserController {
 
     /**
-     * 文件上传
+     * 传统文件上传
      * @return
      */
     @RequestMapping("/fileupload1")
     public String fileupload1(HttpServletRequest request) throws Exception{
-        System.out.println("文件上传。。。");
+        System.out.println("传统文件上传。。。");
 
         // 使用 fileupload 组件完成文件上传
         // 上传的位置
@@ -57,6 +58,37 @@ public class UserController {
                 item.delete();
             }
         }
+
+        return "success";
+    }
+
+    /**
+     * SpringMVC 文件上传
+     * @return
+     */
+    @RequestMapping("/fileupload2")
+    public String fileupload2(HttpServletRequest request, MultipartFile upload) throws Exception{
+        System.out.println("SpringMVC 文件上传。。。");
+
+        // 使用 fileupload 组件完成文件上传
+        // 上传的位置
+        String path = request.getSession().getServletContext().getRealPath("/uploads/");
+
+        // 判断，该路径是否存在
+        File file = new File(path);
+        if (!file.exists()) {
+            // 创建文件夹
+            file.mkdirs();
+        }
+
+        // 说明上传文件项
+        // 获取上传文件的名称
+        String filename = upload.getOriginalFilename();
+        // 把文件名称设置为唯一值，uuid
+        String uuid = UUID.randomUUID().toString().replace("-","");
+        filename = uuid + "_" + filename;
+        // 完成文件上传
+        upload.transferTo(new File(path, filename));
 
         return "success";
     }
--- springmvc_day02_fileupload/target/springmvc_day02_fileupload/index.jsp
+++ springmvc_day02_fileupload/target/springmvc_day02_fileupload/index.jsp
@@ -12,9 +12,16 @@
 </head>
 <body>
 
-    <h3>文件上传</h3>
+    <h3>传统文件上传</h3>
 
     <form action="user/fileupload1" method="post" enctype="multipart/form-data">
+        选择文件：<input type="file" name="upload"><br/>
+        <input type="submit" value="上传" />
+    </form>
+
+    <h3>SpringMVC 文件上传</h3>
+
+    <form action="user/fileupload2" method="post" enctype="multipart/form-data">
         选择文件：<input type="file" name="upload"><br/>
         <input type="submit" value="上传" />
     </form>

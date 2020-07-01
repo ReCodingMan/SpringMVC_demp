e7fa9f799b2a981c24516db0c75060b3f1455106
chengcheng1021 <chengcheng1021@aliyun.com>
2020-07-01 08:58:55 +0800

    拦截器三个方法，配置多个拦截器

--- springmvc_day02_interceptor/src/main/java/com/cc/interceptor/MyInterceptor1.java
+++ springmvc_day02_interceptor/src/main/java/com/cc/interceptor/MyInterceptor1.java
@@ -1,6 +1,7 @@
 package com.cc.interceptor;
 
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,8 +23,35 @@ public class MyInterceptor1 implements HandlerInterceptor {
      */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println("MyInterceptor1 执行了。。。");
-
+        System.out.println("MyInterceptor1 执行了。。。前111");
+        // 转发
+        //request.getRequestDispatcher("/WEB-INF/pages/error.jsp").forward(request, response);
         return true;
+    }
+
+    /**
+     * 后处理方法，controller 方法执行后，success.jsp 执行之前
+     * @param request
+     * @param response
+     * @param handler
+     * @param modelAndView
+     * @throws Exception
+     */
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("MyInterceptor1 执行了。。。后111");
+    }
+
+    /**
+     * success.jsp页面执行后，该方法会执行
+     * @param request
+     * @param response
+     * @param handler
+     * @param ex
+     * @throws Exception
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("success.jsp 执行了。。。后111");
     }
 }

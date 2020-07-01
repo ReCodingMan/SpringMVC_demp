f88ce12c7f210e7fc693999582fe8545749904f3
chengcheng1021 <chengcheng1021@aliyun.com>
2020-07-01 17:35:35 +0800

    整合springmvc框架

--- ssm/src/main/java/com/cc/controller/AccountController.java
+++ ssm/src/main/java/com/cc/controller/AccountController.java
@@ -1,7 +1,22 @@
 package com.cc.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * 账户Web
  */
+@Controller
+@RequestMapping("/account")
 public class AccountController {
+
+    /**
+     * 查询所有账户
+     * @return
+     */
+    @RequestMapping("findAll")
+    public String findAll() {
+        System.out.println("表现层：查询所有账户...");
+        return "list";
+    }
 }
---
+++ ssm/target/ssm/index.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: chengcheng
+  Date: 2020/7/1
+  Time: 5:30 下午
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+    <a href="account/findAll">测试 FindAll</a>
+
+</body>
+</html>

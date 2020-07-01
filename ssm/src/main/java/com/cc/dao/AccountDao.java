1e276e2971190db1e634e79b99a72202a3cdd66a
chengcheng1021 <chengcheng1021@aliyun.com>
2020-07-01 18:15:02 +0800

    ssm整合Mybatis框架

--- ssm/src/main/java/com/cc/dao/AccountDao.java
+++ ssm/src/main/java/com/cc/dao/AccountDao.java
@@ -1,6 +1,8 @@
 package com.cc.dao;
 
 import com.cc.domain.Account;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -13,10 +15,12 @@ public interface AccountDao {
      * 查询所有
      * @return
      */
+    @Select("select * from account")
     public List<Account> findAll();
 
     /**
      * 保存账户
      */
+    @Insert("insert into account(name, money) values(#{name}, #{money})")
     public void saveAccount(Account account);
 }
---
+++ ssm/src/main/java/com/cc/test/TestMybatis.java
@@ -0,0 +1,40 @@
+package com.cc.test;
+
+import com.cc.dao.AccountDao;
+import com.cc.domain.Account;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.util.List;
+
+public class TestMybatis {
+
+    @Test
+    public void run1() throws Exception {
+        // 加载配置文件
+        InputStream is = Resources.getResourceAsStream("SqlMapConfig.xml");
+
+        // 创建 SqlSessionFactory 对象
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is);
+
+        // 创建 SqlSession 对象
+        SqlSession session = factory.openSession();
+
+        // 获取代理对象
+        AccountDao dao = session.getMapper(AccountDao.class);
+
+        //查询所有信息
+        List<Account> accounts = dao.findAll();
+        for (Account account : accounts) {
+            System.out.println(account);
+        }
+
+        //关闭资源
+        session.close();
+        is.close();
+    }
+}
